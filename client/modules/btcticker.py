50b766ba46fb248cfdac46103bae9a2dc98d4cfb
alex <alex.sd3@gmail.com>
2020-02-26 12:47:27 +0100

    dynamic module start

---
+++ client/modules/btcticker.py
@@ -0,0 +1,9 @@
+import json
+import requests
+from . import interval
+
+@interval(15)
+async def btcticker():
+    response = requests.get('https://blockchain.info/ticker')
+    res = response.json()
+    return 'btcticker ' + json.dumps({"value": str(res["USD"]["15m"])})
